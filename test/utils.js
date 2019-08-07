789a9214fe24f946558919505bb72433863e5b54
yAmanda <amandayang@inslabs.org>
2019-08-07 15:27:58 +0800

    feat: update

--- test/utils.js
+++ test/utils.js
@@ -1,54 +1,42 @@
 import {assert} from 'chai'
 import Utils from '../lib/utils'
+import errors from '../lib/errors'
 
-describe('utils_initPassword', () => {
-    it('normal', () => {
-        const passw = '123Abc789'
-        const a = Utils.encodePassword(passw)
-        console.log(a)
-    })
-})
-describe('utils_initPrivate', () => {
+describe('utils_encodePrivate_and_decodePrivate', () => {
     it('normal', () => {
         const priv = '0x59d06271da5276ae03fd2e18fbc78fc91e7405f08d33c5e02cbb74e06d9b322f'
-        const a = Utils.initPrivate(priv, '0xwjfjfkfkkdkk')
-        console.log(a)
+        const encode = Utils.encodePrivate(priv, '0xwjfjfkfkkdkk')
+        const result = Utils.decodePrivate(encode, '0xwjfjfkfkkdkk')
+        assert.equal(result, priv)
     })
-})
-describe('utils_decodePrivate', () => {
-    it('normal', () => {
+    it('decodePrivate_pwd_error', () => {
         const priv = '0x59d06271da5276ae03fd2e18fbc78fc91e7405f08d33c5e02cbb74e06d9b322f'
-        const a = Utils.encodePrivate(priv, '0xwjfjfkfkkdkk')
-        const b = Utils.decodePrivate(a, '0xwjfjfkfkkdkk')
-        console.log('------->', b)
+        const encode = Utils.encodePrivate(priv, '0xwjfjfkfkkdkk')
+        assert.throws(() => {
+            const a = Utils.decodePrivate(encode, '0fjfkdkk')
+            console.log(a)
+        }, errors.DecryptionError())
     })
 })
-describe('utils_verifyPassword', () => {
-    it('normal', () => {
-        const passw = '123Abc789'
-        const a = Utils.initPassword(passw)
-        const b = Utils.verifyPassword(passw)
-        console.log(a)
-    })
-})
+
 describe('utils_getGenerateAccount', () => {
     it('mnemonic_to_account', () => {
         const mnemonic = 'leader verify nut neither motion memory shallow where volcano monkey edge carpet'
-        const a = Utils.generateAccount(mnemonic)
-        console.log(a)
-    })
-    it('no_mnemonic_to_account', () => {
-        const a = Utils.generateAccount()
-        console.log(a)
+        const name = 'demo'
+        const a = Utils.buildAccount(mnemonic, name)
+        assert.equal(a.mnemonic, mnemonic)
     })
     it('mnemonic_is_array', () => {
-        const mnemonic = ['leader', 'nut', 'neither', 'motion', 'memory', 'shallow', 'where', 'volcano', 'monkey', 'edge', 'carpet']
-        const a = Utils.generateAccount(mnemonic)
-        console.log(a)
+        const mnemonic = ['leader', 'verify', 'nut', 'neither', 'motion', 'memory', 'shallow', 'where', 'volcano', 'monkey', 'edge', 'carpet']
+        const name = 'demo'
+        const a = Utils.buildAccount(mnemonic, name)
+        assert.equal(a.addressName, name)
     })
-    it('mnemonic_delete_one', () => {
+    it('mnemonic_only_11_word', () => {
         const mnemonic = 'leader nut neither motion memory shallow where volcano monkey edge carpet'
-        const a = Utils.getGenerateAccount(mnemonic)
-        console.log(a)
+        const name = 'demo'
+        assert.throws(() => {
+            Utils.buildAccount(mnemonic, name)
+        }, errors.MnemonicLengthError())
     })
 })
--- test/walletInterface/index.js
+++ test/walletInterface/index.js
@@ -2,18 +2,115 @@ import {assert} from 'chai'
 import LemoWallet from '../../lib/index'
 
 const testStorage = {
-    memory: {},
+    memory: {
+        ACCOUNT_LIST: [{
+            privKey: 'U2FsdGVkX18xmx1tZrv89IFDJMVcTh38YbBMQFRdk4YmXLfILFUjIft7Yo27BvmgY/muNqmsXmYB2dGXu6yJC/eRxUf9vT4kdxdqZ/XZdSrNBUONYtoucD7b3WRWqVih',
+            address: 'Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ',
+            mnemonic: ['minor', 'half', 'image', 'census', 'endless', 'save', 'wreck', 'fork', 'key', 'wear', 'famous', 'mail'],
+            addressName: 'demoWallet',
+            path: 'm/44\'/60\'/0\'/0',
+        }],
+        PASSWORD_HASH: '8574dcb5f69e1303cdaf0dc8eae797a9bdc5af09f3e8784165165e93a0019c10', // 123AbC789
+    },
     setItem(k, v) {
-        this.memory[k] = v
+        if (k === 'account_list') {
+            this.memory.ACCOUNT_LIST = v
+        }
+        if (k === 'password_hash') {
+            this.memory.PASSWORD_HASH = v
+        }
     },
     getItem(k) {
-        return this.memory
+        return k === 'account_list' ? this.memory.ACCOUNT_LIST : this.memory.PASSWORD_HASH
     },
 }
-// demo
-describe('demo', () => {
-    it('empty demo', () => {
-        const a = LemoWallet
-        assert.equal(a, LemoWallet)
+
+const wallet = new LemoWallet()
+wallet.storage = testStorage
+
+// 保存密码
+describe('utils_savePassword', () => {
+    it('save success', () => {
+        const passw = '123AbC789'
+        const result = wallet.savePassword(passw)
+        assert.equal(result, testStorage.memory.PASSWORD_HASH)
+    })
+})
+// 创建账户
+describe('create_account', () => {
+    it('normal', () => {
+        const password = '123AbC789'
+        const a = wallet.createAccount(password, 'demoWallet')
+        assert.equal(a, testStorage.memory.ACCOUNT_LIST[1])
+    })
+})
+// 导入助记词
+describe('import_mnemonic', () => {
+    it('empty_hhh', () => {
+        const password = '123AbC789'
+        const mnemonic = ['minor', 'half', 'image', 'census', 'endless', 'save', 'wreck', 'fork', 'key', 'wear', 'famous', 'mail']
+        const name = 'hello'
+        const a = wallet.importMnemonic(mnemonic, password, name)
+        assert.equal(a.address, testStorage.memory.ACCOUNT_LIST[0].address)
+    })
+})
+// 导入私钥
+describe('import_private', () => {
+    it('empty_hhh', () => {
+        const password = '123AbC789'
+        const priv = '0xb16043f818288c75627feb6ec52eb6246bfbf5dda2ce0055e499850423919a32'
+        const name = 'hhh'
+        const a = wallet.importPrivate(priv, password, name)
+        console.log(a)
+    })
+})
+// 导出助记词
+describe('export_mnemonic', () => {
+    it('empty_hhh', () => {
+        const password = '123AbC789'
+        const result = wallet.exportMnemonic('Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ', password)
+        assert.equal(result, testStorage.memory.ACCOUNT_LIST[0].mnemonic)
+    })
+})
+// 导出私钥
+describe('export_private', () => {
+    it('empty_hhh', () => {
+        const password = '123AbC789'
+        const result = wallet.exportPrivateKey('Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ', password)
+        console.log(result) // 0xb16043f818288c75627feb6ec52eb6246bfbf5dda2ce0055e499850423919a32
+        assert.equal(result.mnemonic, undefined)
+    })
+})
+// 获取账户列表
+describe('get_account_list', () => {
+    it('normal', () => {
+        const result = wallet.getAccountList()
+        assert.equal(result, testStorage.memory.ACCOUNT_LIST)
+    })
+})
+// 交易签名
+describe('tx_sign', () => {
+    it('normal', () => {
+        const txConfig = {
+            chainID: 100,
+            from: 'Lemo83YZQ7Z77QS744FWSD4BQ3YBCF222ACTWFRQ',
+            to: 'Lemo83GN72GYH2NZ8BA729Z9TCT7KQ5FC3CR6DJG',
+            amount: '1000',
+        }
+        const password = '123AbC789'
+        let a = wallet.sign('Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ', txConfig, password)
+        a = JSON.parse(a)
+        assert.equal(a.from, txConfig.from)
+        assert.equal(a.amount, txConfig.amount)
+    })
+})
+// 修改密码
+describe('modify_password', () => {
+    it('modify_success', () => {
+        const oldPwd = '123AbC789'
+        const newPwd = '111111111'
+        const result = wallet.modifyPassword(oldPwd, newPwd)
+        console.log(result)
+        // assert.equal(result, testStorage.memory.ACCOUNT_LIST)
     })
 })

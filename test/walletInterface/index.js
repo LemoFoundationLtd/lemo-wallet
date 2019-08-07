5033791bca9612cd3ac37a10f2c026e1f11fec24
lnk <wyy102489@outlook.com>
2019-08-07 22:40:10 +0800

    refactor: better code structure

--- test/walletInterface/index.js
+++ test/walletInterface/index.js
@@ -1,28 +1,28 @@
 import {assert} from 'chai'
 import LemoWallet from '../../lib/index'
+import {ACCOUNT_LIST, PASSWORD_HASH} from '../../lib/const'
 
 const testStorage = {
     memory: {
-        ACCOUNT_LIST: [{
+        [ACCOUNT_LIST]: [{
             privKey: 'U2FsdGVkX18xmx1tZrv89IFDJMVcTh38YbBMQFRdk4YmXLfILFUjIft7Yo27BvmgY/muNqmsXmYB2dGXu6yJC/eRxUf9vT4kdxdqZ/XZdSrNBUONYtoucD7b3WRWqVih',
             address: 'Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ',
             mnemonic: ['minor', 'half', 'image', 'census', 'endless', 'save', 'wreck', 'fork', 'key', 'wear', 'famous', 'mail'],
             addressName: 'demoWallet',
             path: 'm/44\'/60\'/0\'/0',
         }],
-        PASSWORD_HASH: '8574dcb5f69e1303cdaf0dc8eae797a9bdc5af09f3e8784165165e93a0019c10', // 123AbC789
+        [PASSWORD_HASH]: '8574dcb5f69e1303cdaf0dc8eae797a9bdc5af09f3e8784165165e93a0019c10', // 123AbC789
     },
     setItem(k, v) {
-        if (k === 'account_list') {
-            this.memory.ACCOUNT_LIST = v
-        }
-        if (k === 'password_hash') {
-            this.memory.PASSWORD_HASH = v
-        }
+        this.memory[k] = copyObj(v)
     },
     getItem(k) {
-        return k === 'account_list' ? this.memory.ACCOUNT_LIST : this.memory.PASSWORD_HASH
+        return copyObj(this.memory[k])
     },
+}
+
+function copyObj(v) {
+    return JSON.parse(JSON.stringify(v))
 }
 
 const wallet = new LemoWallet()
@@ -33,15 +33,15 @@ describe('utils_savePassword', () => {
     it('save success', () => {
         const passw = '123AbC789'
         const result = wallet.savePassword(passw)
-        assert.equal(result, testStorage.memory.PASSWORD_HASH)
+        assert.equal(result, testStorage.memory[PASSWORD_HASH])
     })
 })
 // 创建账户
 describe('create_account', () => {
     it('normal', () => {
         const password = '123AbC789'
-        const a = wallet.createAccount(password, 'demoWallet')
-        assert.equal(a, testStorage.memory.ACCOUNT_LIST[1])
+        const a = wallet.createAccount('demoWallet', password)
+        assert.equal(a, testStorage.memory[ACCOUNT_LIST][1])
     })
 })
 // 导入助记词
@@ -50,8 +50,8 @@ describe('import_mnemonic', () => {
         const password = '123AbC789'
         const mnemonic = ['minor', 'half', 'image', 'census', 'endless', 'save', 'wreck', 'fork', 'key', 'wear', 'famous', 'mail']
         const name = 'hello'
-        const a = wallet.importMnemonic(mnemonic, password, name)
-        assert.equal(a.address, testStorage.memory.ACCOUNT_LIST[0].address)
+        const a = wallet.importMnemonic(mnemonic, name, password)
+        assert.equal(a.address, testStorage.memory[ACCOUNT_LIST][0].address)
     })
 })
 // 导入私钥
@@ -60,7 +60,7 @@ describe('import_private', () => {
         const password = '123AbC789'
         const priv = '0xb16043f818288c75627feb6ec52eb6246bfbf5dda2ce0055e499850423919a32'
         const name = 'hhh'
-        const a = wallet.importPrivate(priv, password, name)
+        const a = wallet.importPrivate(priv, name, password)
         console.log(a)
     })
 })
@@ -69,7 +69,7 @@ describe('export_mnemonic', () => {
     it('empty_hhh', () => {
         const password = '123AbC789'
         const result = wallet.exportMnemonic('Lemo83B5737DA39JJHYJF8DBZ468S4GRAW7BNFAJ', password)
-        assert.equal(result, testStorage.memory.ACCOUNT_LIST[0].mnemonic)
+        assert.equal(result, testStorage.memory[ACCOUNT_LIST][0].mnemonic)
     })
 })
 // 导出私钥
@@ -85,7 +85,7 @@ describe('export_private', () => {
 describe('get_account_list', () => {
     it('normal', () => {
         const result = wallet.getAccountList()
-        assert.equal(result, testStorage.memory.ACCOUNT_LIST)
+        assert.equal(result, testStorage.memory[ACCOUNT_LIST])
     })
 })
 // 交易签名
@@ -111,6 +111,6 @@ describe('modify_password', () => {
         const newPwd = '111111111'
         const result = wallet.modifyPassword(oldPwd, newPwd)
         console.log(result)
-        // assert.equal(result, testStorage.memory.ACCOUNT_LIST)
+        // assert.equal(result, testStorage.memory[ACCOUNT_LIST])
     })
 })
